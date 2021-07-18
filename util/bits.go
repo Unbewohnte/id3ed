9d94b9a8573bee8ab8e64aea8d9e1b5502af4221
Unbewohnte <unbewohnte@pop-os.localdomain>
2021-07-18 13:38:47 +0300

    Fixed utils

--- util/bits.go
+++ util/bits.go
@@ -3,7 +3,7 @@ package util
 // Tells if bit is set in given byte,
 // if bitN <= 0 - always returns false
 func GetBit(b byte, bitN int) bool {
-	if bitN == 0 {
+	if bitN <= 0 {
 		return false
 	}
 	return b&byte(1<<bitN-1) != 0
@@ -12,7 +12,7 @@ func GetBit(b byte, bitN int) bool {
 // Sets bit to 1 in provided byte, if bitN <= 0
 // returns original b without modifications
 func SetBit(b byte, bitN int) byte {
-	if bitN == 0 {
+	if bitN <= 0 {
 		return b
 	}
 	return b | byte(1<<byte(bitN)-1)
